386ac353e8a15e8109bb9bbedc25324ef860a1cb
luoyusang <luoyusang2007@hotmail.com>
2018-08-30 09:18:18 +0800

    narrow-sideBar

--- in_content/in_content.js
+++ in_content/in_content.js
@@ -1,20 +1,34 @@
 
-    var hide_timer;
-    var dragging_on_content = false;
+var hide_timer;
+var dragging_on_content = false;
+
+
+
 
 document.addEventListener('DOMContentLoaded', function(){
 
     
     console.log('STUPID被执行了！');
-    content_div = $("<div id = 'STUPID_content_div'>123</div>");
-    content_div.css('position','fixed');
-    content_div.css('height','100px');
-    content_div.css('width','100px');
-    content_div.css('top','0');
-    content_div.css('left','0');
-    content_div.css('z-index','99999999');
-    content_div.css('background-color','red');
+    content_div = $("<div></div>");
+    content_div.addClass("injected_chrome_extention   ghost_drop");
+    content_div.load(chrome.extension.getURL("/in_content/in_content.html"));
+    
+    
+    
     content_div.hide();
+    
+/*     title_div = $("<div>Title</div>");
+    title_div.addClass("title_div");
+    
+    prompt_div = $("<div>Prompt</div>");
+    prompt_div.addClass("prompt_div");
+    
+    buttons_container_div = $("<div>Prompt</div>");
+    buttons_container_div.addClass("buttons_container_div"); */
+    
+    //content_div.append(title_div).append(prompt_div);
+
+    
     if (window.frames.length == parent.frames.length) {  
 
 
@@ -28,21 +42,20 @@ document.addEventListener('DOMContentLoaded', function(){
         });
         
         $("body").on("dragover",function(evt){
-            //console.log(evt);
-            //$("#STUPID_content_div").stop();
-            //console.log($("#STUPID_content_div").queue().length)
-            content_div.css("background-color","red");
+            //console.log(chrome.extension.getURL("123"));
+            //prompt_div.html("拖至边栏");
+            content_div.removeClass("active");;
             dragging_on_content = false;
             
             
-            console.log(dragging_on_content);
+            //console.log(dragging_on_content);
             if(!content_div.queue().length)
                 content_div.fadeIn(200);
             clearTimeout(hide_timer);
             hide_timer = setTimeout(function(){
                 if(!dragging_on_content)
                     content_div.fadeOut(200);
-            },1000);
+            },500);
             evt.preventDefault();
         });
  //       $("body").on("dragleave",function(evt){
@@ -50,13 +63,13 @@ document.addEventListener('DOMContentLoaded', function(){
  //
  //       });
         content_div.on("dragover",function(evt){
-            content_div.css("background-color","gray");
+            content_div.addClass("active");
             dragging_on_content = true;
             evt.preventDefault();
         });
         content_div.on("dragleave",function(evt){
             
-            content_div.css("background-color","red");
+            content_div.removeClass("active");
             dragging_on_content = false;
             
 /*             hide_timer = setTimeout(function(){
@@ -70,10 +83,13 @@ document.addEventListener('DOMContentLoaded', function(){
             dt = dt.getData('Text');
             content_div.html(dt);
             
+            window.open("//www.baidu.com/s?wd="+dt);    
+            window.open("//www.bing.com/search?q="+dt);  
+            window.open("//www.sogou.com/web?query="+dt);   
             hide_timer = setTimeout(function(){
-                if(!dragging_on_content)
+                if(true)
                     content_div.fadeOut(200);
-            },1500);
+            },800);
             evt.preventDefault();
         });
     }
